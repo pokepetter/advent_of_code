dc36b76d40413b0891a3f16671a9eca1243ff339
pokepetter <pokepetter@gmail.com>
2022-12-01 21:23:16 +0100

    day 1.

---
+++ 2022/1/main.py
@@ -0,0 +1,9 @@
+with open('input.txt', 'r') as f:
+    bags = [[int(e) for e in bag.split('\n') if e] for bag in f.read().split('\n\n')] # convert each paragraph to a list of numbers
+    bags = [sum(e) for e in bags]
+    bags.sort(reverse=True)
+
+    print(f'the biggest bag has {bags[0]} kcal')
+
+    sum_of_three_biggest_bags = sum(bags[:3])
+    print(f'the three biggest bag has a total of {sum_of_three_biggest_bags} kcal')
