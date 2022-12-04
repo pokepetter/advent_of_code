43cbcb358da713ccfe4a69b35cffe17ff22512a9
pokepetter <pokepetter@gmail.com>
2022-12-04 13:17:36 +0100

    day 4.

---
+++ 2022/4.py
@@ -0,0 +1,23 @@
+with open('4_input.txt', 'r') as f:
+    all_pairs = [l for l in f.read().split('\n') if l]
+
+pairs_with_complete_overlap = 0
+pairs_with_some_overlap = 0
+
+for pair in all_pairs:
+    pair = pair.split(',')
+
+    for i, assignment in enumerate(pair):
+        assignment = assignment.split('-')
+        assignment = [int(e) for e in assignment]
+        assignment = tuple(range(assignment[0], assignment[1]+1))   # convert start and end to a tuple of all the numbers
+        pair[i] = assignment
+
+    first, second = pair
+    # check if second contains all of first or first contains all of second and increment if it does
+    pairs_with_complete_overlap += int(all(e in first for e in second) or all(e in second for e in first))
+    # same, but with any overlap
+    pairs_with_some_overlap += int(any(e in first for e in second) or any(e in second for e in first))
+
+print(pairs_with_complete_overlap)
+print(pairs_with_some_overlap)
