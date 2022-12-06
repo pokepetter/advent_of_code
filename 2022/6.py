dbc16f27fc37601b279a4f7336ae6251aa1370e5
Petter Amland <petter@rain-games.com>
2022-12-06 12:24:07 +0100

    day 6.

--- 2022/6.py
+++ 2022/6.py
@@ -1,14 +1,12 @@
 with open('6_input.txt', 'r') as f:
     text = f.read()
 
-
 def find_marker_with_length(n):
     for i in range(0, len(text)-n):
         if len(set(text[i:i+n])) == n:
             break
 
     return i + n
-
 
 print(find_marker_with_length(4))
 print(find_marker_with_length(14))
